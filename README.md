855f594f4fd1909d8f999aae0458cd433ae1059b
Colin Reid <cpreid2@gmail.com>
2018-06-19 09:13:04 -0500

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CTA-Tracker
+
+Code that generates a live Chicago CTA Train Tracker using Bokeh.
+
+Live Train Tracker App: https://chi-cta-train-tracker.herokuapp.com
+
+![Train Tracker](images/Train_Tracker.png)
--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+
+# Live CTA Train Tracker
+
 from flask import render_template, jsonify, request
 from flask import Flask
 
@@ -13,20 +16,25 @@ from ast import literal_eval
 
 from bokeh.plotting import figure
 from bokeh.tile_providers import CARTODBPOSITRON
-from bokeh.models import ColumnDataSource, CategoricalColorMapper, HoverTool
+from bokeh.models import ColumnDataSource, CategoricalColorMapper, HoverTool, WheelZoomTool, CDSView, BooleanFilter
 from bokeh.models.sources import AjaxDataSource
 from bokeh.layouts import gridplot
+import shapefile
 
+# Create pages
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello World!'
-
-@app.route('/dashboard/')
 def show_dashboard():
     plots = []
     plots.append(make_plot())
+
+    return render_template('layout.html', plots=plots)
+
+@app.route('/mobile')
+def show_dashboard_mobile():
+    plots = []
+    plots.append(make_plot_mobile())
 
     return render_template('layout.html', plots=plots)
 
@@ -44,23 +52,11 @@ def merc(Coords):
 # Plot Generator
 def make_plot():
 
-    CTA_Lines = ['Red','G','Blue','P','Brn','Pink','Org','Y']
-
-    source = AjaxDataSource(data_url=request.url_root + 'cta_data/', polling_interval=5000, mode='replace')
-    source.data = dict(x=[],y=[],next_station=[],destination=[],direction=[],color=[])
-
-
-    color_mapper = CategoricalColorMapper(factors= CTA_Lines, palette=['Red','Green','Blue','Purple','Brown','Pink','Orange','Yellow'])
-
-    hover = HoverTool(tooltips=[
-        ("Next Stop", "@next_station"),
-        ("Destination","@destination")
-
-    ])
-
-    plot = figure(title='Live CTA Tracker',x_range=(-9780000, -9745000), y_range=(5130000, 5160000),x_axis_type="mercator", y_axis_type="mercator",tools=[hover, 'wheel_zoom','pan'],sizing_mode='stretch_both')
+    # Initialize Plot
+    plot = figure(x_range=(-9790000, -9745000), y_range=(5120000, 5170000),x_axis_type="mercator", y_axis_type="mercator",tools=['pan'],sizing_mode='stretch_both')
     plot.add_tile(CARTODBPOSITRON)
 
+    plot.toolbar.active_scroll = "auto"
     plot.xaxis.major_tick_line_color = None  # turn off x-axis major ticks
     plot.xaxis.minor_tick_line_color = None  # turn off x-axis minor ticks
 
@@ -70,20 +66,187 @@ def make_plot():
     plot.xaxis.major_label_text_font_size = '0pt'  # turn off x-axis tick labels
     plot.yaxis.major_label_text_font_size = '0pt'  # turn off y-axis tick labels
 
-    circles = plot.circle(x='x',y='y', source=source,size=9,color={'field': 'color', 'transform': color_mapper})
+    plot.toolbar.logo = None
+    plot.toolbar_location = None
+
+    # Read in train line data
+    sf = shapefile.Reader("CTA_New/CTA_New")
+    features = sf.shapeRecords()
+
+    Lines = []
+    Coords_x = []
+    Coords_y = []
+    for shape in features:
+        Line = shape.record[5]
+        Coord = shape.shape.points
+        X_s = []
+        Y_s = []
+        for coord in Coord:
+            Trans = merc(tuple(reversed(coord)))
+            X_s.append(Trans[0])
+            Y_s.append(Trans[1])
+        Coords_x.append(X_s)
+        Coords_y.append(Y_s)
+
+    CTA_Lines = ['Red','G','Blue','P','Brn','Pink','Org','Y']
+
+    # Set up data sources
+        # - Live CTA Data
+    source = AjaxDataSource(data_url=request.url_root + 'cta_data/', polling_interval=5000, mode='replace')
+    source.data = dict(x=[],y=[],next_station=[],destination=[],direction=[],color=[],line_name=[])
+
+        # - Station Coordinate Data
+    L_Map = pd.read_csv('Stations.csv')
+    station_source = ColumnDataSource(dict(x=L_Map['coords_x'],y=L_Map['coords_y'],name=L_Map['STATION_NAME']))
+
+    # Color Map for trains
+    color_mapper = CategoricalColorMapper(factors= CTA_Lines, palette=['Red','Green','Blue','Purple','Brown','Pink','Orange','Yellow'])
+
+    # Plot Glyphs
+    for i in range(len(Coords_x)):
+        plot.line(x=Coords_x[i],y=Coords_y[i],line_color="black",alpha=0.7)
+
+    stations = plot.circle(x = 'x', y = 'y', source=station_source,size=5, line_color="black", fill_color = 'white')
+    circles = plot.circle(x='x',y='y',angle='heading',source=source,color={'field': 'color', 'transform': color_mapper},size=14,line_color="black",line_width=0.8, legend='line_name')
+    triangles = plot.triangle(x='x',y='y',angle='heading',source=source,size=8,color='white')
+
+    # Set Up Tools
+    hover = HoverTool(tooltips=[
+        ("Next Stop", "@next_station"),
+        ("Destination","@destination")
+    ],renderers=[circles])
+
+    station_hover = HoverTool(tooltips=[
+        ("Station", "@name")
+    ],renderers=[stations])
+
+    wheel = WheelZoomTool()
+
+    plot.add_tools(hover)
+    plot.add_tools(station_hover)
+    plot.add_tools(wheel)
+
+    plot.toolbar.active_scroll = wheel
+
+    plot.legend.location = "top_left"
 
     script, div = components(plot)
     return script, div
 
+# Plot Generator
+def make_plot_mobile():
+
+    # Initialize Plot
+    plot = figure(x_range=(-9790000, -9745000), y_range=(5120000, 5170000),x_axis_type="mercator", y_axis_type="mercator",tools=['pan'],sizing_mode='stretch_both')
+    plot.add_tile(CARTODBPOSITRON)
+
+    plot.toolbar.active_scroll = "auto"
+    plot.xaxis.major_tick_line_color = None  # turn off x-axis major ticks
+    plot.xaxis.minor_tick_line_color = None  # turn off x-axis minor ticks
+
+    plot.yaxis.major_tick_line_color = None  # turn off y-axis major ticks
+    plot.yaxis.minor_tick_line_color = None  # turn off y-axis minor ticks
+
+    plot.xaxis.major_label_text_font_size = '0pt'  # turn off x-axis tick labels
+    plot.yaxis.major_label_text_font_size = '0pt'  # turn off y-axis tick labels
+
+    plot.toolbar.logo = None
+    plot.toolbar_location = None
+
+    # Read in train line data
+    sf = shapefile.Reader("CTA_New/CTA_New")
+    features = sf.shapeRecords()
+
+    Lines = []
+    Coords_x = []
+    Coords_y = []
+    for shape in features:
+        Line = shape.record[5]
+        Coord = shape.shape.points
+        X_s = []
+        Y_s = []
+        for coord in Coord:
+            Trans = merc(tuple(reversed(coord)))
+            X_s.append(Trans[0])
+            Y_s.append(Trans[1])
+        Coords_x.append(X_s)
+        Coords_y.append(Y_s)
+
+    CTA_Lines = ['Red','G','Blue','P','Brn','Pink','Org','Y']
+
+    # Set up data sources
+        # - Live CTA Data
+    source = AjaxDataSource(data_url=request.url_root + 'cta_data/', polling_interval=5000, mode='replace')
+    source.data = dict(x=[],y=[],next_station=[],destination=[],direction=[],color=[],line_name=[])
+
+        # - Station Coordinate Data
+    L_Map = pd.read_csv('Stations.csv')
+    station_source = ColumnDataSource(dict(x=L_Map['coords_x'],y=L_Map['coords_y'],name=L_Map['STATION_NAME']))
+
+    # Color Map for trains
+    color_mapper = CategoricalColorMapper(factors= CTA_Lines, palette=['Red','Green','Blue','Purple','Brown','Pink','Orange','Yellow'])
+
+    # Plot Glyphs
+    for i in range(len(Coords_x)):
+        plot.line(x=Coords_x[i],y=Coords_y[i],line_color="black",alpha=1)
+
+    stations = plot.circle(x = 'x', y = 'y', source=station_source,size=10, line_color="black", fill_color = 'white')
+    circles = plot.circle(x='x',y='y',angle='heading',source=source,color={'field': 'color', 'transform': color_mapper},size=28,line_color="black",line_width=0.8, legend='line_name')
+    triangles = plot.triangle(x='x',y='y',angle='heading',source=source,size=16,color='white')
+
+    # Set Up Tools
+    hover = HoverTool(tooltips="""
+    <div>
+    	<div>
+    		<span style="font-size: 24px; font-weight: bold;">Next Stop: </span>
+            <span style="font-size: 24px;">@next_station</span>
+    	</div>
+    	<div> 
+    		<span style="font-size: 24px; font-weight: bold;">Destination: </span>
+            <span style="font-size: 24px;">@destination</span>
+        </div>
+    </div>
+    """,renderers=[circles])
+
+    station_hover = HoverTool(tooltips="""
+    <div>
+    	<div>
+    		<span style="font-size: 24px; font-weight: bold;">Station: </span>
+            <span style="font-size: 24px;">@name</span>
+    	</div>
+    </div>
+    """,renderers=[stations])
+
+    wheel = WheelZoomTool()
+
+    plot.add_tools(hover)
+    plot.add_tools(station_hover)
+    plot.add_tools(wheel)
+
+    plot.toolbar.active_scroll = wheel
+
+    plot.legend.location = "top_left"
+
+    script, div = components(plot)
+    return script, div
 
 # Data endpoint to fetch CTA train data from the API
 @app.route('/cta_data/', methods=['GET','POST'])
 def data():
     CTA_Lines = ['Red','G','Blue','P','Brn','Pink','Org','Y']
 
+    equiv = {'Red':'Red Line',
+    'G':'Green Line',
+    'Blue':'Blue Line',
+    'P':'Purple Line',
+    'Brn':'Brown Line',
+    'Pink':'Pink Line',
+    'Org':'Orange Line',
+    'Y':'Yellow Line'}
+
     Train_Data = []
     for line in CTA_Lines:
-        r = requests.get('http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=XYZ&rt='+line+'&outputType=JSON')
+        r = requests.get('http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=973d5cc84055445e80c56d967c4a2bec&rt='+line+'&outputType=JSON')
         data = json.loads(r.text)
         try:
             trains = data['ctatt']['route'][0]['train']
@@ -98,10 +261,15 @@ def data():
     AllTrainData['coords_x'] = AllTrainData[['lat','lon']].apply(lambda x: merc(x)[0], axis=1)
     AllTrainData['coords_y'] = AllTrainData[['lat','lon']].apply(lambda x: merc(x)[1], axis=1)
     AllTrainData.fillna('',inplace=True)
+    AllTrainData['Line_Name'] = AllTrainData['Color'].map(equiv)
 
     return jsonify(x=list(AllTrainData['coords_x']),
                    y=list(AllTrainData['coords_y']),
                    next_station=list(AllTrainData['nextStaNm']),
                    destination=list(AllTrainData['destNm']),
-                   direction=list(AllTrainData['heading']),
+                   line_name=list(AllTrainData['Line_Name']),
+                   heading=[math.radians(x) for x in list(AllTrainData['heading'])],
                    color=list(AllTrainData['Color']))
+
+if __name__ == "__main__":
+	app.run()
